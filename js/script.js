5ec9693dcdde87ccb556c974d243f0d64870df79
jason490 <116290832+jason490@users.noreply.github.com>
2022-11-03 14:27:04 -0400

    Added nightlight

--- js/script.js
+++ js/script.js
@@ -1,29 +1,5 @@
 
-(function (global){
-
-    var navbarURL = "snippets/navbarSnippet.html";
-    var insertHtml = function (selector, html){
-        var targetElem = document.querySelector(selector);
-        targetElem.innerHTML = html
-    };
-    var showLoading = function(selector){
-        var html = "<div class ='text-center'";
-        html += "Loading...</div>";
-        insertHtml(selector,html)
-    };
-    var insertProperty = function (string, propName, propValue) {
-        var propToReplace = "{{" + propName + "}}";
-        string = string
-          .replace(new RegExp(propToReplace, "g"), propValue);
-        return string;
-    };
-    document.addEventListener("DOMContentLoaded", function (event){
-        showLoading("#main-content");
-        $ajaxUtils.sendGetRequest(
-            navbarURL,
-            buildAndShowHomeHTML,
-            true);
-    });
-
-})(window);
-
+// $( window ).load(function() {
+//     $('#thing').load('snippets/navbarSnippet.html')
+// });
+$('#thing').load('snippets/navbarSnippet.html')
